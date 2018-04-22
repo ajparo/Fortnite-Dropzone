0a9062d641375359b8e26e27ea92edad460ead4f
Grant Maloney <gpmpn2@mail.missouri.edu>
2018-04-22 16:31:17 -0500

    Initial product

--- Fortnite Dropzone/ViewController.swift
+++ Fortnite Dropzone/ViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainscreenLabel: UILabel!
+    @IBOutlet weak var mainscreenLabelLower: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "Background")
+        backgroundImage.contentMode =  UIViewContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+        
+        mainscreenLabel.text = "Fortnite"
+        mainscreenLabelLower.text = "Dropzone"
+        
+        var clockTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(update), userInfo: nil, repeats: false)
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = .clear
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc
+    func update() {
+        self.performSegue(withIdentifier: "moveToMain", sender: nil)
     }
 
 
